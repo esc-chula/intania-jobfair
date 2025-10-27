b69f17f95985280bdd3619c926cda1e70bdfaeeb
tawanorkchiengtai <tawan.09@hotmail.com>
2025-10-27 16:44:35 +0700

    feat: add LogoPreview component for enhanced image display

---
+++ components/common/image-preview.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
+
+type Props = {
+  src: string;
+  alt: string;
+  thumbContainerClassName?: string; // size/position classes for thumbnail wrapper
+  imageClassName?: string; // classes for the <Image> element
+  modalHeightClassName?: string; // height of preview container, default h-[70vh]
+};
+
+export function LogoPreview({
+  src,
+  alt,
+  thumbContainerClassName = "relative w-16 h-16",
+  imageClassName = "object-contain rounded-md bg-white p-1",
+  modalHeightClassName = "h-[70vh]",
+}: Props) {
+  // Note: modalHeightClassName is kept for compatibility; react-photo-view manages its own layout.
+  return (
+    <div className={thumbContainerClassName}>
+      <PhotoProvider maskOpacity={0.5} speed={() => 300} easing={() => "ease-out"}>
+        <PhotoView src={src}>
+          {/* Using next/image as the thumbnail; PhotoView will open the lightbox on click */}
+          <Image src={src} alt={alt} fill className={`${imageClassName} cursor-zoom-in`} />
+        </PhotoView>
+      </PhotoProvider>
+    </div>
+  );
+}
+
+export default LogoPreview;
