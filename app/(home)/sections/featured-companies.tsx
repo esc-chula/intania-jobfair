53e31b6a8a9ec8ae45f59f84cd2bdb59a1dd2e4f
WatthanakornR <watthanakorn.r@gmail.com>
2026-01-05 00:13:16 +0700

    show 6 card companies

--- app/(home)/sections/featured-companies.tsx
+++ app/(home)/sections/featured-companies.tsx
@@ -11,7 +11,7 @@ export default async function FeaturedCompanies() {
       fetchJobs(),
     ]);
 
-    const featuredCompanies = pickFeaturedCompanies(companies, 4);
+    const featuredCompanies = pickFeaturedCompanies(companies, 6);
 
     return (
       <Section
@@ -44,6 +44,8 @@ export default async function FeaturedCompanies() {
         actionHref="/companies"
       >
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <SkeletonCard />
+          <SkeletonCard />
           <SkeletonCard />
           <SkeletonCard />
           <SkeletonCard />
