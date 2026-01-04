4738dbce9278a08d2e8741a99751d3331717201d
frostnzx <jeensiwakorn001@gmail.com>
2026-01-04 18:18:26 +0700

    feat: add date filter to companies and jobs page

--- components/jobs/job-list.tsx
+++ components/jobs/job-list.tsx
@@ -6,11 +6,11 @@ import SearchBar from "./search-bar";
 import SortSelector from "./sort-select";
 import JobCard from "./job-card";
 import type {
-  Job,
-  Company,
-  EligibleStudentYear,
-  MajorEligibility,
-  PositionType,
+    Job,
+    Company,
+    EligibleStudentYear,
+    MajorEligibility,
+    PositionType,
 } from "@/types/schema";
 import FilterSelector from "./filter-select";
 import GroupedFilterSelector from "./group-filter-select";
@@ -18,193 +18,223 @@ import { RefreshCw } from "lucide-react";
 
 // 👇 import options ที่เราแยกออกมา
 import {
-  positionTypeOptions,
-  jobTypeOptions,
-  eligibleYearOptions,
-  groupedMajorOptions,
+    positionTypeOptions,
+    dateOptions,
+    jobTypeOptions,
+    eligibleYearOptions,
+    groupedMajorOptions,
 } from "@/constants/job-filter-options";
 
 export default function JobsListClient({
-  initialJobs,
-  initialCompanies,
-  cardsPerPage = 10,
-}: {
-  initialJobs: Job[];
-  initialCompanies: Company[];
-  cardsPerPage?: number;
-}) {
-  const [sortOption, setSortOption] = useState<
-    "position" | "open-date" | "close-date"
-  >("position");
-  const [page, setPage] = useState(1);
-  const [query, setQuery] = useState("");
-  const [isFilterOpen, setIsFilterOpen] = useState(false);
-  const [positionTypeFilter, setPositionTypeFilter] = useState<string>("");
-  const [jobTypeFilter, setJobTypeFilter] = useState<string>("");
-  const [eligibleYearFilter, setEligibleYearFilter] = useState<string>("");
-  const [majorFilter, setMajorFilter] = useState<string>("");
-
-  const searchedJobs = useMemo(() => {
-    return initialJobs.filter((job) => {
-      const company =
-        initialCompanies.find((c) => c[""] === job.companyId) ?? null;
-
-      // 1. Search query
-      const matchesQuery =
-        job.jobTitle.toLowerCase().includes(query.toLowerCase()) ||
-        (company &&
-          company.companyName_th.toLowerCase().includes(query.toLowerCase()));
-
-      // 2. Position type filter
-      const matchesPositionType =
-        positionTypeFilter === "" ||
-        positionTypeFilter === "All" ||
-        job.positionType === positionTypeFilter;
-
-      // 3. Job type filter
-      const matchesJobType =
-        jobTypeFilter === "" ||
-        jobTypeFilter === "All" ||
-        job.field_of_work.includes(jobTypeFilter);
-
-      // 4. Eligible year filter
-      const matchesEligibleYear =
-        eligibleYearFilter === "" ||
-        eligibleYearFilter === "All" ||
-        job.eligibleStudentYear[
-          eligibleYearFilter as keyof EligibleStudentYear
-        ] == true;
-
-      // 5. Major filter
-      const matchesMajor =
-        majorFilter === "" ||
-        majorFilter === "All" ||
-        job.major[majorFilter as string] === true;
-
-      // combine all
-      return (
-        matchesQuery &&
-        matchesPositionType &&
-        matchesJobType &&
-        matchesEligibleYear &&
-        matchesMajor
-      );
-    });
-  }, [
     initialJobs,
     initialCompanies,
-    query,
-    positionTypeFilter,
-    jobTypeFilter,
-    eligibleYearFilter,
-    majorFilter,
-  ]);
+    cardsPerPage = 10,
+}: {
+    initialJobs: Job[];
+    initialCompanies: Company[];
+    cardsPerPage?: number;
+}) {
+    const [sortOption, setSortOption] = useState<
+        "position" | "open-date" | "close-date"
+    >("position");
+    const [page, setPage] = useState(1);
+    const [query, setQuery] = useState("");
+    const [isFilterOpen, setIsFilterOpen] = useState(false);
+    const [positionTypeFilter, setPositionTypeFilter] = useState<string>("");
+    const [jobTypeFilter, setJobTypeFilter] = useState<string>("");
+    const [eligibleYearFilter, setEligibleYearFilter] = useState<string>("");
+    const [majorFilter, setMajorFilter] = useState<string>("");
+    const [dateFilter, setDateFilter] = useState("");
 
-  const sortedJobs = useMemo(() => {
-    const arr = [...searchedJobs];
-    if (sortOption === "position")
-      return arr.sort((a, b) => a.jobTitle.localeCompare(b.jobTitle));
+    const searchedJobs = useMemo(() => {
+        return initialJobs.filter((job) => {
+            const company =
+                initialCompanies.find((c) => c[""] === job.companyId) ?? null;
 
-    if (sortOption === "open-date")
-      return arr.sort(
-        (a, b) =>
-          new Date(a.application_start || "").getTime() -
-          new Date(b.application_start || "").getTime(),
-      );
+            // 1. Search query
+            const matchesQuery =
+                job.jobTitle.toLowerCase().includes(query.toLowerCase()) ||
+                (company &&
+                    company.companyName_th
+                        .toLowerCase()
+                        .includes(query.toLowerCase()));
 
-    if (sortOption === "close-date")
-      return arr.sort(
-        (a, b) =>
-          new Date(a.application_end || "").getTime() -
-          new Date(b.application_end || "").getTime(),
-      );
+            // 2. Position type filter
+            const matchesPositionType =
+                positionTypeFilter === "" ||
+                positionTypeFilter === "All" ||
+                job.positionType === positionTypeFilter;
 
-    return arr;
-  }, [searchedJobs, sortOption]);
+            // 3. Job type filter
+            const matchesJobType =
+                jobTypeFilter === "" ||
+                jobTypeFilter === "All" ||
+                job.field_of_work.includes(jobTypeFilter);
 
-  const totalPages = Math.max(1, Math.ceil(sortedJobs.length / cardsPerPage));
-  if (page > totalPages) setPage(totalPages);
+            // 4. Eligible year filter
+            const matchesEligibleYear =
+                eligibleYearFilter === "" ||
+                eligibleYearFilter === "All" ||
+                job.eligibleStudentYear[
+                    eligibleYearFilter as keyof EligibleStudentYear
+                ] == true;
 
-  const paginatedJobs = useMemo(() => {
-    const startIndex = (page - 1) * cardsPerPage;
-    return sortedJobs.slice(startIndex, startIndex + cardsPerPage);
-  }, [sortedJobs, page, cardsPerPage]);
+            // 5. Major filter
+            const matchesMajor =
+                majorFilter === "" ||
+                majorFilter === "All" ||
+                job.major[majorFilter as string] === true;
 
-  return (
-    <div className="flex flex-col gap-6">
-      <SearchBar
-        query={query}
-        setQuery={setQuery}
-        isFilterOpen={isFilterOpen}
-        setIsFilterOpen={setIsFilterOpen}
-        setPage={setPage}
-      />
-      {isFilterOpen && (
-        <>
-          <FilterSelector
-            filterOption={positionTypeFilter}
-            setFilterOption={(v) => {
-              (setPositionTypeFilter(v), setPage(1));
-            }}
-            options={positionTypeOptions}
-            placeholder="เลือกรูปแบบการทำงาน"
-          />
-          <FilterSelector
-            filterOption={jobTypeFilter}
-            setFilterOption={setJobTypeFilter}
-            options={jobTypeOptions}
-            placeholder="เลือกสายงานของตำแหน่ง"
-          />
-          <FilterSelector
-            filterOption={eligibleYearFilter}
-            setFilterOption={setEligibleYearFilter}
-            options={eligibleYearOptions}
-            placeholder="เลือกระดับการศึกษา"
-          />
-          <GroupedFilterSelector
-            filterOption={majorFilter}
-            setFilterOption={setMajorFilter}
-            groupedOptions={groupedMajorOptions}
-            placeholder="เลือกสาขาวิชา"
-          />
-          {/* Reset filters button */}
-          <div className="flex justify-center">
-            <button
-              onClick={() => {
-                setPositionTypeFilter("");
-                setJobTypeFilter("");
-                setEligibleYearFilter("");
-                setMajorFilter("");
-                setPage(1);
-              }}
-              className="text-sm flex gap-2 font-bodyTH text-[#D9A94C]"
-            >
-              <RefreshCw /> ล้างตัวกรอง
-            </button>
-          </div>
-        </>
-      )}
 
-      <SortSelector sortOption={sortOption} setSortOption={setSortOption} />
-      <h2 className="heading-th-2 text-primary-blue">ตำแหน่งงานทั้งหมด</h2>
 
-      <div className="flex flex-col gap-4 items-center">
-        {paginatedJobs.map((job) => {
-          const company =
-            initialCompanies.find((c) => c[""] === job.companyId) ?? null;
-          return (
-            <div key={job.jobId} className="w-full">
-              <JobCard job={job} company={company} />
+            // Apply booth date filter
+            const matchesDate =
+                dateFilter === "" ||
+                dateFilter === "All" ||
+                (dateFilter == "1" && company!.boothDay1 != "") ||
+                (dateFilter == "2" && company!.boothDay2 != "");
+
+            // combine all
+            return (
+                matchesQuery &&
+                matchesPositionType &&
+                matchesJobType &&
+                matchesEligibleYear &&
+                matchesMajor && 
+                matchesDate
+            );
+        });
+    }, [
+        initialJobs,
+        initialCompanies,
+        query,
+        positionTypeFilter,
+        jobTypeFilter,
+        eligibleYearFilter,
+        majorFilter,
+        dateFilter
+    ]);
+
+    const sortedJobs = useMemo(() => {
+        const arr = [...searchedJobs];
+        if (sortOption === "position")
+            return arr.sort((a, b) => a.jobTitle.localeCompare(b.jobTitle));
+
+        if (sortOption === "open-date")
+            return arr.sort(
+                (a, b) =>
+                    new Date(a.application_start || "").getTime() -
+                    new Date(b.application_start || "").getTime()
+            );
+
+        if (sortOption === "close-date")
+            return arr.sort(
+                (a, b) =>
+                    new Date(a.application_end || "").getTime() -
+                    new Date(b.application_end || "").getTime()
+            );
+
+        return arr;
+    }, [searchedJobs, sortOption]);
+
+    const totalPages = Math.max(1, Math.ceil(sortedJobs.length / cardsPerPage));
+    if (page > totalPages) setPage(totalPages);
+
+    const paginatedJobs = useMemo(() => {
+        const startIndex = (page - 1) * cardsPerPage;
+        return sortedJobs.slice(startIndex, startIndex + cardsPerPage);
+    }, [sortedJobs, page, cardsPerPage]);
+
+    return (
+        <div className="flex flex-col gap-6">
+            <SearchBar
+                query={query}
+                setQuery={setQuery}
+                isFilterOpen={isFilterOpen}
+                setIsFilterOpen={setIsFilterOpen}
+                setPage={setPage}
+            />
+            {isFilterOpen && (
+                <>
+                    <FilterSelector
+                        filterOption={dateFilter}
+                        setFilterOption={(v) => {
+                            setDateFilter(v);
+                            setPage(1);
+                        }}
+                        options={dateOptions}
+                        placeholder="เลือกวันที่เข้าร่วมงาน"
+                    />
+                    <FilterSelector
+                        filterOption={positionTypeFilter}
+                        setFilterOption={(v) => {
+                            (setPositionTypeFilter(v), setPage(1));
+                        }}
+                        options={positionTypeOptions}
+                        placeholder="เลือกรูปแบบการทำงาน"
+                    />
+                    <FilterSelector
+                        filterOption={jobTypeFilter}
+                        setFilterOption={setJobTypeFilter}
+                        options={jobTypeOptions}
+                        placeholder="เลือกสายงานของตำแหน่ง"
+                    />
+                    <FilterSelector
+                        filterOption={eligibleYearFilter}
+                        setFilterOption={setEligibleYearFilter}
+                        options={eligibleYearOptions}
+                        placeholder="เลือกระดับการศึกษา"
+                    />
+                    <GroupedFilterSelector
+                        filterOption={majorFilter}
+                        setFilterOption={setMajorFilter}
+                        groupedOptions={groupedMajorOptions}
+                        placeholder="เลือกสาขาวิชา"
+                    />
+                    {/* Reset filters button */}
+                    <div className="flex justify-center">
+                        <button
+                            onClick={() => {
+                                setPositionTypeFilter("");
+                                setJobTypeFilter("");
+                                setEligibleYearFilter("");
+                                setMajorFilter("");
+                                setPage(1);
+                            }}
+                            className="text-sm flex gap-2 font-bodyTH text-[#D9A94C]"
+                        >
+                            <RefreshCw /> ล้างตัวกรอง
+                        </button>
+                    </div>
+                </>
+            )}
+
+            <SortSelector
+                sortOption={sortOption}
+                setSortOption={setSortOption}
+            />
+            <h2 className="heading-th-2 text-primary-blue">
+                ตำแหน่งงานทั้งหมด
+            </h2>
+
+            <div className="flex flex-col gap-4 items-center">
+                {paginatedJobs.map((job) => {
+                    const company =
+                        initialCompanies.find((c) => c[""] === job.companyId) ??
+                        null;
+                    return (
+                        <div key={job.jobId} className="w-full">
+                            <JobCard job={job} company={company} />
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
 
-      <PaginationControls
-        currentPage={page}
-        totalPages={totalPages}
-        onPageChange={setPage}
-      />
-    </div>
-  );
+            <PaginationControls
+                currentPage={page}
+                totalPages={totalPages}
+                onPageChange={setPage}
+            />
+        </div>
+    );
 }
